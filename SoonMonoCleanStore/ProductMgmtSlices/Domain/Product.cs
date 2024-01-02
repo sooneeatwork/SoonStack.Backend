1b78cb105d9a46bb8727c5412a516828c2c8ea66
soon ee <sooneeatwork@gmail.com>
2024-01-02 21:54:24 +0800

    ADD Product domain & database model Development for product management slices, add product and database model

---
+++ SoonMonoCleanStore/ProductMgmtSlices/Domain/Product.cs
@@ -0,0 +1,40 @@
+﻿using SharedKernal.Domain;
+using System;
+
+namespace ProductMgmtSlices.Domain
+{
+    public class Product : BaseEntity
+    {
+        // Properties specific to Product
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int StockQuantity { get; private set; }
+       
+
+      
+
+        // Public constructor
+        public Product(string name, 
+                       string description, 
+                       decimal price, 
+                       int stockQuantity, 
+                       long? createdBy = null, 
+                       long? modifiedBy = null)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            StockQuantity = stockQuantity;
+            CreatedBy = createdBy.GetValueOrDefault();
+            ModifiedBy = modifiedBy.GetValueOrDefault();
+        }
+
+        // Methods to update the entity
+        public void UpdateStock(int quantity)
+        {
+            // Implement logic to update stock
+            StockQuantity = quantity;
+        }
+    }
+}
---
+++ SoonMonoCleanStore/ProductMgmtSlices/Repository/DatabaseModel/ProductTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProductMgmtSlices.Repository.DatabaseModel
+{
+    public class ProductTable
+    {
+        // Corresponding to the database fields
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+     
+
+        // Constructor for initializing a new instance
+        public ProductTable(string name, string description, decimal price, int stockQuantity, int? createdBy = null, int? modifiedBy = null, DateTime? createdDate = null, DateTime? modifiedDate = null)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            StockQuantity = stockQuantity;
+            CreatedBy = createdBy;
+            ModifiedBy = modifiedBy;
+            CreatedDate = createdDate ?? DateTime.UtcNow;
+            ModifiedDate = modifiedDate ?? DateTime.UtcNow;
+        }
+
+        // Additional methods if needed for database-related operations
+        // ...
+    }
+}
